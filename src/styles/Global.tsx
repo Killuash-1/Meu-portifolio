a820c24c29a1ec7b8f622dad738b28a1df5c6812
Killuash-1 <killuash-1@localhost.localdomain>
2023-04-02 13:35:54 -0300

    feat: portifolio

--- src/styles/Global.tsx
+++ src/styles/Global.tsx
@@ -84,7 +84,7 @@ export const GlobalStyle = ({ children }: GlobalStyleProps): JSX.Element => {
 
   const theme = createTheme({
     colors: {
-      brand1: "#623CEA",
+      brand1: "#17723a",
       brand2: "#311E75",
 
       grey0: "#0A0A0B",
--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
-  instagramUser: "samuelleaoui",
+  nameUser: "Kauan Fernandes Pires",
+  githubUser: "killuash-1",
+  linkedinUser: "kauanfernandes",
+  whatsappNumber: "+5513981460122",
+  emailUser: "kauanfp56@gmail.com",
+  instagramUser: "kauan.f.p",
   facebookUser: "",
 };
